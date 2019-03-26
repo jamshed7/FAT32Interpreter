55f0ebca749c01577fd495cf527166ef9a8f09aa
Jamshed Jahangir <jamshed@Jamsheds-MacBook-Air.local>
2019-03-26 14:11:36 -0500

    added file open and file close

--- mfs.cpp
+++ mfs.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <stdio.h>
+#include <fstream>
 #include <boost/algorithm/string.hpp>
 #include <vector>
 #include <csignal>
@@ -9,6 +11,7 @@
 #define maxCommandSize 255
 #define maxNumArguments 5
 #define whitespace " \t\n"
+
 
 std::ifstream FAT32Img;
 
@@ -20,14 +23,37 @@ int main(){
     std::signal(SIGINT, signalHandler);
     std::signal(SIGTSTP, signalHandler);
 
+    std::ifstream inputFile;
+
     while(true){
         std::cout << "mfs>";
         std::getline(std::cin,commandLineInput);
         std::vector<std::string> tokenizedInput;
         boost::split(tokenizedInput, commandLineInput, boost::is_any_of(whitespace));
+
         if(tokenizedInput[0] == "exit" || tokenizedInput[0] == "quit"){
             exit(0);
         }
+
+        if(tokenizedInput[0] == "open")
+        {
+          inputFile.open(tokenizedInput[1]);
+
+          if(inputFile == NULL)
+          {
+            printf("Error: FAT32 image file not found!\n");
+          }
+          else
+          {
+            printf("FAT32 image file sucessfully opened.\n");
+          }
+        }
+
+        if (tokenizedInput[0] == "close")
+        {
+          inputFile.close();
+        }
+
     }
     return 0;
 }
