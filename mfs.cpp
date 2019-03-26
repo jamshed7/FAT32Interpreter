b708ae0f521e69008c52e244a21bebef03fe92e7
Jamshed Jahangir <jamshed@Jamsheds-MacBook-Air.local>
2019-03-26 14:53:30 -0500

    implemented part of 'info' command

--- mfs.cpp
+++ mfs.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
-#include <stdio.h>
-#include <fstream>
 #include <boost/algorithm/string.hpp>
 #include <vector>
 #include <csignal>
 #include <fstream>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
 
 #define maxCommandSize 255
 #define maxNumArguments 5
 #define whitespace " \t\n"
 
 
-std::ifstream FAT32Img;
+//std::ifstream FAT32Img;
+std::FILE* fp;
+
+uint16_t BPB_RsvdSecCnt;
+uint16_t BPB_BytsPerSec;
+uint8_t BPB_SecPerClus;
+uint32_t BPB_FATSz32;
+uint8_t BPB_NumFATs;
+
 
 void signalHandler(int signum);
 
@@ -23,7 +35,6 @@ int main(){
     std::signal(SIGINT, signalHandler);
     std::signal(SIGTSTP, signalHandler);
 
-    std::ifstream inputFile;
 
     while(true){
         std::cout << "mfs>";
@@ -31,15 +42,22 @@ int main(){
         std::vector<std::string> tokenizedInput;
         boost::split(tokenizedInput, commandLineInput, boost::is_any_of(whitespace));
 
+
+        //Exit command
         if(tokenizedInput[0] == "exit" || tokenizedInput[0] == "quit"){
             exit(0);
         }
 
+        //Open file command
         if(tokenizedInput[0] == "open")
         {
-          inputFile.open(tokenizedInput[1]);
+          int n = tokenizedInput[1].length();
+          char inputFilename[n + 1];
+          strcpy(inputFilename, tokenizedInput[1].c_str());
 
-          if(inputFile == NULL)
+          fp = std::fopen(inputFilename, "r");
+
+          if(fp == NULL)
           {
             printf("Error: FAT32 image file not found!\n");
           }
@@ -49,9 +67,19 @@ int main(){
           }
         }
 
+        //Close file command
         if (tokenizedInput[0] == "close")
         {
-          inputFile.close();
+          std::fclose(fp);
+        }
+
+        //Info Command
+        if( tokenizedInput[0] == "info")
+        {
+          fseek(fp, 11, SEEK_SET);
+          fread(&BPB_BytsPerSec, 2, 1, fp);
+
+          printf("BPB_BytsPerSec: %d \t %x\n", BPB_BytsPerSec, BPB_BytsPerSec);
         }
 
     }
