1b822997d5ccb31ab015840c77dcf3417e9dc9e9
Adarsh Pai <adarsh9pai@gmail.com>
2019-03-26 15:19:37 -0500

    .

--- mfs.cpp
+++ mfs.cpp
@@ -5,12 +5,15 @@
 #include <vector>
 #include <csignal>
 #include <fstream>
+#include <cstdint>
 
 #define maxCommandSize 255
 #define maxNumArguments 5
 #define whitespace " \t\n"
 
 std::ifstream FAT32Img;
+
+
 
 void signalHandler(int signum);
 
@@ -27,6 +30,9 @@ int main(){
         boost::split(tokenizedInput, commandLineInput, boost::is_any_of(whitespace));
         if(tokenizedInput[0] == "exit" || tokenizedInput[0] == "quit"){
             exit(0);
+        }
+        if(tokenizedInput[0] == "open"){
+            FAT32Img.open(tokenizedInput[1]);
         }
     }
     return 0;
